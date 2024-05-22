9c4b6360d260b9169e05b5b45659d69f59afaf14
maxzreis <maxzdosreis@gmail.com>
2024-05-21 22:38:56 -0300

    Primeiro Commit

---
+++ DesafioControleFluxo/Contador.java
@@ -0,0 +1,31 @@
+package DesafioControleFluxo;
+
+import java.util.Scanner;
+
+public class Contador{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro:");
+        int parametroUm = scanner.nextInt();
+        System.out.println("Digite o segundo parâmetro:");
+        int parametroDois = scanner.nextInt();
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println(e.getMessage());
+        } catch (NumberFormatException e){
+            System.out.println("Ambos o snúmeros devem ser números inteiros");
+        }
+    }
+
+    public static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if(parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+            int contagem = parametroDois - parametroUm;
+            for (int i = 1; i <= contagem; i++) {
+                System.out.println("Imprimindo o número \n" + i);
+            
+        } 
+    }
+}
